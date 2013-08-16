ef943ceed03942c88964ddeaa0a7881f10677408
Jason McVetta <jason.mcvetta@gmail.com>
2013-08-16 16:16:15 -0700

    another path

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	restful "github.com/emicklei/go-restful"
+	"github.com/jmcvetta/randutil"
 	"log"
 	"net/http"
 )
@@ -15,6 +16,10 @@ type Foo struct {
 	Crew []Crew
 }
 
+type Bar struct {
+	Rand string
+}
+
 func getFoo(request *restful.Request, response *restful.Response) {
 	f := Foo{
 		Crew: []Crew{
@@ -26,12 +31,20 @@ func getFoo(request *restful.Request, response *restful.Response) {
 
 }
 
+func getBar(request *restful.Request, response *restful.Response) {
+	var b Bar
+	b.Rand, _ = randutil.AlphaString(8)
+	response.WriteEntity(&b)
+}
+
 func main() {
 	baseUrl := ":8080"
 	var ws restful.WebService
 	ws.Path("/api").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
+	ws.Route(ws.GET("/bar").To(getBar).
+		Writes(Bar{}))
 	ws.Route(ws.GET("/foo").To(getFoo).
 		Writes(Foo{}))
 	restful.Add(&ws)
